4d5713d69fea640bb85e8d3e07fc9e72fe383fce
otdigital-soft <ot.digitalsoft77@gmail.com>
2023-02-01 21:59:42 -0500

    add env

---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+// Path: ./config/env/production/server.js
+
+module.exports = ({ env }) => ({
+  proxy: true,
+  url: env("APP_URL"), // Sets the public URL of the application.
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
